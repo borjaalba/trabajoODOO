2e700c2dc32eaaafc7cd625d6642e001e7948bfe
daniel frias <danfribal@alum.us.es>
2023-03-31 18:31:09 +0200

    modelos

--- prueba/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-class Ruta(models.Model):
-    _name = 'ruta'
-    _description = 'Módulo para generar rutas en Google Maps'
-    start_point = fields.Char('Punto de inicio')
-    end_point = fields.Char('Punto de fin')
-    waypoints = fields.Char('Puntos intermedios')
---
+++ prueba/models/ruta.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+class Ruta(models.Model):
+    _name = 'Rutas.ruta'
+    _description = 'Módulo para generar rutas en Google Maps'
+    origen = fields.Char('Punto de inicio', required=True)
+    destino = fields.Char('Punto de fin', required=True)
+    waypoints = fields.Char('Puntos intermedios')
+    distancia = fields.Float('Distancia', readonly=True)
+    duracion = fields.Float('Duración', readonly=True)
